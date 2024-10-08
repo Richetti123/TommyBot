b00c067c5ec68cb06d85948cc2484a01d3c60547
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-10-08 02:26:24 +0200

    Update info-creadora.js

--- plugins/info-creadora.js
+++ plugins/info-creadora.js
@@ -6,12 +6,12 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
 try{
 let contact, number, ofc, nombre, description, correo, lugar, enlace, biog
 let pp = gataImg
-const cat = `MichiBot
+const cat = `TommyBot
 
 *---------------------*
 
 *IG*
-*https://www.instagram.com/ceogeripium.dzn*
+*https://www.instagram.com/tommy_98jerry*
 
 Asistencia
 *${asistencia}*
